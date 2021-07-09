99302f6d15ac2ac6054774d9734d3d337c9e7e5d
Victor <victor.ribero3@gmail.com>
2021-07-09 09:36:03 -0600

    updated phone numb,er

--- pages/index.js
+++ pages/index.js
@@ -1,113 +1,130 @@
-import Head from 'next/head'
-import { Layout } from "../client/components/ui/Organism/Layout"
-import { LinkButton } from "../client/components/ui/Atom/LinkButton"
-import { Container } from "../client/components/ui/Atom/Container"
-import { Title, titleSizes, titleTags } from "../client/components/ui/Atom/Title"
-import { RiInstagramLine, RiTwitterLine, RiMessage2Line } from 'react-icons/ri'
-import Video from 'client/components/ui/Atom/Video'
-import { Pricing } from 'client/components/ui/Organism/Home/Pricing'
-import { FindUs } from 'client/components/ui/Organism/Home/FindUs'
-import { About } from 'client/components/ui/Organism/Home/About'
-import { Contact } from 'client/components/ui/Organism/Home/Contact'
-import { Footer } from 'client/components/ui/Organism/Footer'
+import Head from "next/head";
+import { Layout } from "../client/components/ui/Organism/Layout";
+import { LinkButton } from "../client/components/ui/Atom/LinkButton";
+import { Container } from "../client/components/ui/Atom/Container";
+import {
+  Title,
+  titleSizes,
+  titleTags,
+} from "../client/components/ui/Atom/Title";
+import { RiInstagramLine, RiTwitterLine, RiMessage2Line } from "react-icons/ri";
+import Video from "client/components/ui/Atom/Video";
+import { Pricing } from "client/components/ui/Organism/Home/Pricing";
+import { FindUs } from "client/components/ui/Organism/Home/FindUs";
+import { About } from "client/components/ui/Organism/Home/About";
+import { Contact } from "client/components/ui/Organism/Home/Contact";
+import { Footer } from "client/components/ui/Organism/Footer";
 
-function Home () {
-	return (
-		<Layout>
+function Home() {
+  return (
+    <Layout>
       <Head>
-        <title>Centro esports de alto rendimiento en Barcelona | El Templo Esports</title>
-        <meta name='description' content='El Templo Esports es un centro gaming de alto rendimiento en Barcelona con un enfoque 100% a la comunidad. ¡Ven a jugar con tus amigos!'/>
+        <title>
+          Centro esports de alto rendimiento en Barcelona | El Templo Esports
+        </title>
+        <meta
+          name="description"
+          content="El Templo Esports es un centro gaming de alto rendimiento en Barcelona con un enfoque 100% a la comunidad. ¡Ven a jugar con tus amigos!"
+        />
       </Head>
       <HomeHeader />
       {/* <OurBrands/> */}
-			<main>
-        <About/>
+      <main>
+        <About />
         <Pricing />
-        <FindUs/>
-        {/* <Benefits/> */} 
-        <Contact/>
-			</main>
-      <Footer/>
-			
+        <FindUs />
+        {/* <Benefits/> */}
+        <Contact />
+      </main>
+      <Footer />
+
       <style jsx>{`
-         main {
+        main {
           padding-top: 7.5rem;
           background-image: url(/images/wallpaper-malzahar.jpg);
-          background-image: linear-gradient(to bottom, rgba(26,26,46,0.8), rgba(26,26,46,1)), url(/images/wallpaper-malzahar.jpg);
-          background-size: cover;  
+          background-image: linear-gradient(
+              to bottom,
+              rgba(26, 26, 46, 0.8),
+              rgba(26, 26, 46, 1)
+            ),
+            url(/images/wallpaper-malzahar.jpg);
+          background-size: cover;
           background-position: top right;
           background-repeat: no-repeat;
-         }
-			`}</style>
-		</Layout>
-	)
+        }
+      `}</style>
+    </Layout>
+  );
 }
-export default Home
-
-
+export default Home;
 
 function HomeHeader() {
-  const scrollToSelector = elementSelector => {
+  const scrollToSelector = (elementSelector) => {
     var element = document.querySelector(elementSelector);
-    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
-  }
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
 
   const handleClick = (e, selector) => {
-    e.preventDefault()
-    scrollToSelector(selector)
-  }
+    e.preventDefault();
+    scrollToSelector(selector);
+  };
 
   return (
     <section>
-      <div className="home-hero-overlay"/>
+      <div className="home-hero-overlay" />
       <Container>
-        <div className='home-hero-displayer'>
-          <div className='HomeHeader-inner'>
-            <Title
-              size={titleSizes.giant}
-              as={titleTags.h1}
-              spacelessTop>
+        <div className="home-hero-displayer">
+          <div className="HomeHeader-inner">
+            <Title size={titleSizes.giant} as={titleTags.h1} spacelessTop>
               El Templo eSports
             </Title>
-            <p>Te gustan los eSports? Te gusta jugar? Entonces también te gustaremos nosotros. Ven a conocernos!</p>
-            <div className='HomeHeader-buttons'>
+            <p>
+              Te gustan los eSports? Te gusta jugar? Entonces también te
+              gustaremos nosotros. Ven a conocernos!
+            </p>
+            <div className="HomeHeader-buttons">
               <LinkButton
-                href='#find-us'
-                onClick={e => handleClick(e, '#find-us')}
-                theme='primary'
-                size='large'
-                style={{marginRight: '0.5rem'}}>
+                href="#find-us"
+                onClick={(e) => handleClick(e, "#find-us")}
+                theme="primary"
+                size="large"
+                style={{ marginRight: "0.5rem" }}
+              >
                 Donde estamos
               </LinkButton>
               <LinkButton
-                href='#contact'
-                onClick={e => handleClick(e, '#contact')}
-                theme='accent'
-                size='large'>
+                href="#contact"
+                onClick={(e) => handleClick(e, "#contact")}
+                theme="accent"
+                size="large"
+              >
                 Contactanos
               </LinkButton>
             </div>
-          
-            <div className='HomeHeader-tags'>
+
+            <div className="HomeHeader-tags">
               <HomeTagContact
-                icon={<RiInstagramLine size={16}/>}
-                href='https://www.instagram.com/temploesports/'>
+                icon={<RiInstagramLine size={16} />}
+                href="https://www.instagram.com/temploesports/"
+              >
                 temploesports
               </HomeTagContact>
               <HomeTagContact
-                icon={<RiTwitterLine size={16}/>}
-                href='https://twitter.com/TemploEsports'>
+                icon={<RiTwitterLine size={16} />}
+                href="https://twitter.com/TemploEsports"
+              >
                 temploesports
               </HomeTagContact>
               <HomeTagContact
-                icon={<RiMessage2Line size={16}/>}
-                href='tel:+34685478720'>
-                685478720
+                icon={<RiMessage2Line size={16} />}
+                href="tel:+34646502841"
+              >
+                646 502 841
               </HomeTagContact>
             </div>
           </div>
-          <div className='video-wrapper'>
-            <Video src="/videos/el-templo-presentation.mp4" type="video/mp4"/>
+          <div className="video-wrapper">
+            <Video src="/videos/el-templo-presentation.mp4" type="video/mp4" />
           </div>
         </div>
       </Container>
@@ -122,25 +139,30 @@ function HomeHeader() {
           display: flex;
           align-items: center;
         }
-        
+
         .home-hero-displayer {
           display: flex;
           flex-direction: column;
         }
-        
+
         .video-wrapper {
           display: block;
           margin-top: 4rem;
           margin-bottom: -10rem;
-          animation: fromBottom 0.35s ease-in, fadein .5s ease-in;
+          animation: fromBottom 0.35s ease-in, fadein 0.5s ease-in;
           animation-delay: 0.5s;
           animation-fill-mode: forwards;
           opacity: 0;
         }
 
         .home-hero-overlay {
-          background-image: url('https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80');
-          background-image: linear-gradient(180deg, rgba(26,26,46,1), rgba(26,26,46,0.5)), url('https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80');
+          background-image: url("https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
+          background-image: linear-gradient(
+              180deg,
+              rgba(26, 26, 46, 1),
+              rgba(26, 26, 46, 0.5)
+            ),
+            url("https://images.unsplash.com/photo-1542751371-adc38448a05e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80");
           background-size: cover;
           position: absolute;
           height: 100%;
@@ -161,7 +183,7 @@ function HomeHeader() {
             filter: blur(0);
           }
         }
-        
+
         .HomeHeader-inner {
           box-sizing: border-box;
           animation: fromBottom 0.5s ease-in, fadein 0.75s ease-in;
@@ -183,7 +205,7 @@ function HomeHeader() {
             transform: translateX(0);
           }
         }
-  
+
         @keyframes fadein {
           from {
             opacity: 0;
@@ -225,24 +247,22 @@ function HomeHeader() {
             width: 350px;
           }
         }
-    `}</style>
+      `}</style>
     </section>
-  )
+  );
 }
 
-function HomeTagContact({icon, href, children}) {
+function HomeTagContact({ icon, href, children }) {
   return (
     <a href={href}>
-      <span>
-        {icon}
-      </span>
+      <span>{icon}</span>
       {children}
 
       <style jsx>{`
         a {
           align-items: center;
           border-radius: 5px;
-          background-color: rgba(255,255,255, 0.1);
+          background-color: rgba(255, 255, 255, 0.1);
           display: flex;
           font-size: 0.75rem;
           padding: 0.35rem 0.5rem;
@@ -251,21 +271,20 @@ function HomeTagContact({icon, href, children}) {
           transition: background-color 0.15s ease;
         }
 
-        
         a:not(:first-child) {
           margin-left: 0.25rem;
         }
-        
+
         a:hover {
-          background-color: rgba(255,255,255, 0.2);
+          background-color: rgba(255, 255, 255, 0.2);
         }
-        
+
         span {
           display: flex;
           align-items: center;
           margin-right: 0.25rem;
         }
-        
+
         @media screen and (min-width: 768px) {
           a {
             font-size: 0.8rem;
@@ -273,5 +292,5 @@ function HomeTagContact({icon, href, children}) {
         }
       `}</style>
     </a>
-  )
+  );
 }
